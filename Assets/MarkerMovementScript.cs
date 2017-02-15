982bbfd4734ad02f7b8020b7e0c12e113b192a2c
Unknown <n.mason1@outlook.com>
2017-02-14 16:22:45 -0800

    Nick's first commit
    added a movement script to the player's movement marker (using crappy sprites)

---
+++ Assets/MarkerMovementScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarkerMovementScript : MonoBehaviour {
+    public float moveDist;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown("up") || Input.GetKeyDown("w"))
+        {
+            transform.Translate(0, 1, 0);
+        }
+        if (Input.GetKeyDown("down") || Input.GetKeyDown("s"))
+        {
+            transform.Translate(0, -1, 0);
+        }
+        if (Input.GetKeyDown("left") || Input.GetKeyDown("a"))
+        {
+            transform.Translate(-1, 0, 0);
+        }
+        if (Input.GetKeyDown("right") || Input.GetKeyDown("d"))
+        {
+            transform.Translate(1, 0, 0);
+        }
+    }
+}
---
+++ Assets/PlayerMovementScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementScript : MonoBehaviour {
+    Transform target;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxisRaw("Submit") != 0)
+        {
+            //move the player to the x value of the target, then the y value
+        }
+	}
+}
