7b7007ad77a0fe595ad1cb3843804e4398088dd0
Unknown <n.mason1@outlook.com>
2017-02-14 20:55:00 -0800

    Main Menu added
    added a main menu and a placeholder for the scoreboard menu

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour {
+    private Dictionary<string, int> scores;
+	// Use this for initialization
+	void Start () {
+		
+	}
+    void Init()
+    {
+        if (scores == null)
+        {
+            scores = new Dictionary<string, int>();
+        }
+    }
+
+    int GetScore(string player)
+    {
+        Init();
+        if (scores.ContainsKey(player) == false)
+        {
+            return 0;
+        }
+        return scores[player];
+    }
+
+	void SetScore(string player, int value)
+    {
+        Init();
+        scores[player] = value;
+    }
+
+    void IncrementScore(string player)
+    {
+        Init();
+        scores[player] = scores[player] + 1;
+    }
+}
