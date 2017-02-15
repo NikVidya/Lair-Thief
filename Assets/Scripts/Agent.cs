a58a091bd1cdc916b49c1cfab9cd1ad628b7aa1e
Labrasones <labrasones@gmail.com>
2017-02-14 22:40:18 -0800

    Added the basics for turn based movement. Very buggy. Needs a lot of work.

---
+++ Assets/Scripts/Agent.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Agent : MonoBehaviour {
+
+	public bool isTurnOver = true;
+
+	public void TurnStart (){
+		if (isTurnOver) {
+			isTurnOver = false;
+			OnTurnStart ();
+		}
+	}
+
+	public void TurnEnd (){
+		if (!isTurnOver) {
+			isTurnOver = true;
+			OnTurnEnd ();
+		}
+	}
+
+	public void Update(){
+		if (!isTurnOver) {
+			OnTurnUpdate ();
+		}
+	}
+
+	protected virtual void OnTurnUpdate() {}
+	protected virtual void OnTurnStart () {}
+	protected virtual void OnTurnEnd () {}
+}
--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -4,32 +4,63 @@ using UnityEngine;
 
 public class BoardManager : MonoBehaviour {
 
-	public int initialHeight = 10;
+	public int initialHeight = 8;
 	public int columnCount = 5;
 
-	public GameObject[] cellTypes;
+	public Cell[] cellTypes;
 
-	private Queue<GameObject>[] board_data; // An array of Queues of Prefabs. The prefabs should be the cells
+	public float cellScale { get; private set; }
 
+	private List<Cell>[] board_data; // An array of Queues of Prefabs. The prefabs should be the cells
+	private int maxReachableHeight = 8;
+
+	// Returns whether or not the position is traversable
+	public bool IsTraversable(int x, int y){
+		if (x < 0 || x >= columnCount || y < 0 || y >= maxReachableHeight) {
+			return false;
+		} else {
+			return board_data [x] [y].IsPlayerTraversable ();
+		}
+	}
+
+	public void UpdateCellPositions(){
+		for (int i = 0; i < columnCount; i++) {
+			for (int j = 0; j < initialHeight; j++) {
+				Cell cell = board_data [i] [j];
+				cell.transform.position = new Vector2 (i * cellScale, j * cellScale);
+				cell.transform.localScale = new Vector2 (cellScale, cellScale);
+			}
+		}
+	}
+
+	public Vector2 CellToWorldPos(int x, int y){
+		return new Vector2 (x * cellScale, y * cellScale);
+	}
+
+	public int[] WorldToCellPos(float pixX, float pixY){
+		Vector3 worldPos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		return new int[2] { (int)Mathf.Floor (worldPos.x / cellScale), (int)Mathf.Floor (worldPos.y / cellScale) };
+	}
 
 	// Use this for initialization
 	void Start () {
 		// Initialize the board
+		cellScale = (Camera.main.orthographicSize * 2.0f * Camera.main.aspect) / (float)columnCount;
 
-		float cellScale = (Camera.main.orthographicSize * 2.0f * Camera.main.aspect) / (float)columnCount;
+		maxReachableHeight = (int)Mathf.Floor( (float) ( (Camera.main.orthographicSize * 2.0f) / cellScale) );
 
-		board_data = new Queue<GameObject>[columnCount];
+		board_data = new List<Cell>[columnCount];
 		for (int i = 0; i < columnCount; i++) {
-			board_data [i] = new Queue<GameObject> ();
+			board_data [i] = new List<Cell> ();
 			for (int j = 0; j < initialHeight; j++) {
-				GameObject cell = Instantiate (cellTypes[Constants.Board.Enums.GROUND_CELL], transform) as GameObject;
+				Cell cell = (Cell) Instantiate (cellTypes[Constants.Board.Enums.GROUND_CELL], transform);
 				cell.transform.position = new Vector2 (i * cellScale, j * cellScale);
 				cell.transform.localScale = new Vector2 (cellScale, cellScale);
-				board_data[i].Enqueue (cell);
+				board_data[i].Add(cell);
 			}
 		}
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		
