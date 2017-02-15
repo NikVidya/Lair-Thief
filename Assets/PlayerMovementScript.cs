b3e8c01ee5c7458df47ddd15e380dc7f979ed792
Unknown <n.mason1@outlook.com>
2017-02-14 17:03:10 -0800

    Player movement initial
    player moves horizontally to  the target's x position when enter is pressed

--- Assets/PlayerMovementScript.cs
+++ Assets/PlayerMovementScript.cs
@@ -3,17 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovementScript : MonoBehaviour {
-    Transform target;
+    public Transform target;
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetAxisRaw("Submit") != 0)
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxisRaw("Submit") != 0)
         {
             //move the player to the x value of the target, then the y value
+            while (transform.position.x != target.position.x)
+            {
+                transform.position = Vector2.MoveTowards(new Vector2(transform.position.x, transform.position.y), new Vector2(target.position.x, transform.position.y), Time.deltaTime * 1);
+            }
         }
-	}
+    }
 }
