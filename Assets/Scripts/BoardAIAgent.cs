efb6a58a8ba5ed5d7d832b57900151965bb93dbb
Labrasones <labrasones@gmail.com>
2017-02-16 10:08:16 -0800

    Implemented path checker. Before the board AI finishes it's turn, it makes a valid path for the player traverse through the level.

--- Assets/Scripts/BoardAIAgent.cs
+++ Assets/Scripts/BoardAIAgent.cs
@@ -3,16 +3,114 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BoardAIAgent : Agent {
-	public BoardManager board;
 
-	public TextAsset[] patternSources;
+	// -----------------------
 
-	private List<List<List<Cell.CellType>>> patterns = new List<List<List<Cell.CellType>>>();
+	public BoardManager board; // The board this AI agent operates on
+	public PlayerAgent player; // The player agent this AI is aware of
+
+	// -----------------------
+
+	public TextAsset[] patternSources; // The source .csv files to use to generate new patterns
+
+	private List<List<List<Cell.CellType>>> patterns = new List<List<List<Cell.CellType>>>(); // The in memory pattern pallet
+
 
 	public void Start(){ // Parse all the patterns into data
 		for (int i = 0; i < patternSources.Length; i++) {
 			patterns.Add( ParsePatternFile(patternSources[i]) );
 		}
+	}
+
+
+	//		Psuedocode
+	//
+	//		function ValidatePathFromCell(Cell cell) {
+	//			if( cell.y == board.top ){
+	//				return true;
+	//			}
+	//			int xLeft = cell.x, xRight = cell.x;
+	//			Cell[] cells;
+	//			// Find reachable cells to the left
+	//			while( xLeft > 0 && board[xLeft][cell.y] is passable ){
+	//				cells.Add(board[xLeft])
+	//				xLeft--;
+	//			}
+	//			// Find reachable cells to the right
+	//			while( xRight < board.columns && board[xRight][cell.y] is passable ){
+	//				cells.Add(board[xRight])
+	//				xRight++;
+	//			}
+	//
+	//			// Find which cells we can move up to
+	//			Cell[] upReachCells;
+	//			for(i=0; i<cells.Count; i++){
+	//				if( board[cells[i].x][cells[i].y+1] is passable ){
+	//					upReachCells.Add(board[cells[i].x][cells[i].y+1])
+	//				}
+	//			}
+	//
+	//			if( upReachCells.Count == 0) {
+	//				// Pick a random cell from cells to clear a path from
+	//				Cell randCell = cells[Random.Range(0,cells.Count)];
+	//				board.ChangeCell(randCell.x, randCell.y+1, Passable);
+	//				upReachCells.Add(randCell);
+	//			}
+	//
+	//			for(int i=0; i<upReachCells.Count; i++){
+	//				ValidatePathFromCell(upReachCells[i]);
+	//			}
+	//		}
+
+	private bool MakeValidPath(Cell fromCell){
+		if (fromCell == null) {
+			return false; // You reached a null cell. Probably the player is above the highest cells. That should only happen during initial generation.
+		}
+		if (fromCell.y >= board.boardHeight) {
+			return true; // The path reached a cell in the top row, this is a valid path
+		}
+		int x = 0;
+
+		List<Cell> rowCells = new List<Cell>();
+		// fromCell is reachable, push that one
+		rowCells.Add(fromCell);
+
+		// Collect reachable cells to the left of fromCell
+		x = fromCell.x - 1; // Start with the cell immediately to the left of the player.
+		while (x >= 0 && board.IsTraversable (x, fromCell.y)) {
+			rowCells.Add (board.GetCell (x, fromCell.y));
+			x--; // Move to the left
+		}
+
+		// Collect reachable cells to the right of fromCell
+		x = fromCell.x + 1; // Start with the cell immediately to the right of the player.
+		while (x < board.columns && board.IsTraversable (x, fromCell.y)) {
+			rowCells.Add (board.GetCell (x, fromCell.y));
+			x++; // Move to the right
+		}
+
+		// Now, find the cells which have a path upwards
+		List<Cell> columnCells = new List<Cell>();
+		for (int i = 0; i < rowCells.Count; i++) {
+			if (board.IsTraversable (rowCells [i].x, rowCells [i].y + 1)) { // Check if the cell above this one is traversable
+				columnCells.Add (board.GetCell (rowCells [i].x, rowCells [i].y + 1));
+			}
+		}
+
+		// Check if we need to make the path valid
+		if (columnCells.Count <= 0) {
+			// Choose a random cell to turn into a path
+			Cell randCell = rowCells[ Random.Range(0, rowCells.Count) ];
+			Cell newColCell = board.ChangeCell (randCell.x, randCell.y + 1, Cell.CellType.NONE );
+			columnCells.Add (newColCell); // Add this to the list of cells to make a valid path for.
+		}
+
+		// TODO One could add a bit of randomization here. Clear a path above a random row cell, but don't add it to columnCells. It won't be guarenteed to be a path, and could be a misdirect.
+
+		// Choose a random columnCells to further validate
+		// TODO Choose multiple columnCells? Multiple paths that have enforced validation?
+		// Recurse along the path
+		return MakeValidPath( columnCells[ Random.Range(0, columnCells.Count) ] );
 	}
 
 	protected override void OnTurnStart(){
@@ -22,6 +120,10 @@ public class BoardAIAgent : Agent {
 		while (board.NeedsRow (5)) {
 			board.PushCellPattern (MakePattern ());
 		}
+
+		// Make sure there is a valid path!
+		MakeValidPath( board.GetCell(player.cellPosX, player.cellPosY) );
+
 
 		TurnEnd ();
 	}
