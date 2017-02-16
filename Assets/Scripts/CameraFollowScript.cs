6fc0281423f5c5cc81a949976352063baa641d6c
Unknown <n.mason1@outlook.com>
2017-02-15 23:33:29 -0800

    Added endangered movement
    Made the check if the pos y is less than 2, and if it is bool[,] variables change to the appropriate movement region constant. Also made the camera background green.

---
+++ Assets/Scripts/CameraFollowScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowScript : MonoBehaviour {
+    public Transform target;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(transform.position.x != target.position.x && transform.position.y != target.position.y)
+        {
+            transform.position = new Vector3(target.position.x, target.position.y, -10);
+        }
+	}
+}
---
+++ Assets/Scripts/ExitGameScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitGameScript : MonoBehaviour {
+    // Update is called once per frame
+    void Update()
+    {
+        // checks if key Escape is pressed and loads up the main menu if it is
+        if (Input.GetKey(KeyCode.Escape))
+        {
+           SceneManager.LoadScene("Menu");
+        }
+    }
+}
