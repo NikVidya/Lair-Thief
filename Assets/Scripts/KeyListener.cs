c78aecf9e9d75315727bd698589d6451f5e750f7
Unknown <n.mason1@outlook.com>
2017-02-17 19:35:53 -0800

    Added key input
    there's a key listener for ending turn and using the powerups, also the player moves with keys now

---
+++ Assets/Scripts/KeyListener.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyListener : MonoBehaviour {
+    public PlayerAgent player;
+    public BoostPowerup boost;
+    public BreakerPowerup breaker;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space))
+        {
+            player.TurnEnd();
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            boost.Activate();
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            breaker.Activate();
+        }
+
+	}
+}
