03f28ab3c29b22c5217b896ac9e1856d0304c9d7
Soulist <soulistpvp@gmail.com>
2017-02-18 21:40:38 -0800

    Fixed Everything To Match A 16:9 Aspect Ratio
    Done

---
+++ Assets/Editor/Reset.cs
@@ -0,0 +1,13 @@
+﻿ using UnityEditor;
+ using UnityEngine;
+ 
+ public class AddMenu : EditorWindow {
+ 
+     [MenuItem("Edit/Reset Playerprefs")]
+ 
+     public static void DeletePlayerPrefs()
+     {
+         PlayerPrefs.DeleteAll();
+     }
+ 
+ }
